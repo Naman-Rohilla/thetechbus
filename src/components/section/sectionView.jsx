ab83a74ef7b340d25bd37dedbd52016bf7f503bf
naman-rohilla-kr <naman.rohilla@kroger.com>
2024-10-14 15:25:38 +0530

    Updated the animation

--- src/components/section/sectionView.jsx
+++ src/components/section/sectionView.jsx
@@ -1,68 +1,36 @@
-import { useEffect, useRef, useState } from "react";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 import "./sectionView.scss"; // Add styles for the section
 
 const SectionView = ({ id, backgroundColor, heading, children }) => {
-  const [isVisible, setIsVisible] = useState(false);
   const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const observedId = entry.target.getAttribute("id");
-          if (observedId === id) {
-            if (entry.isIntersecting) {
-              setIsVisible(true); // Section is visible
-            } else {
-              setIsVisible(false); // Section is out of view
-            }
-          }
-        });
-      },
-      {
-        threshold: 0.01, // Adjust this threshold to trigger earlier/later
-      }
-    );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
-      }
-    };
-  }, [id]);
+  const isInView = useInView(sectionRef, { threshold: 0.01 });
 
   return (
-    <>
-      <section
-        id={id}
-        style={{
-          "--background-color": backgroundColor,
-        }}
-        className={`section-container `}
-        ref={sectionRef}
+    <motion.section
+      id={id}
+      style={{
+        "--background-color": backgroundColor,
+      }}
+      className="section-container"
+      ref={sectionRef}
+      initial={{ opacity: 0, y: 100 }}
+      animate={isInView ? { opacity: 1, y: 0 } : { y: -100 }}
+      transition={{ duration: 0.8 }}
+    >
+      <motion.div className="section-heading">
+        <span>{heading}</span>
+      </motion.div>
+
+      <motion.div
+        className="section-content"
+        initial={{ opacity: 0 }}
+        animate={isInView ? { opacity: 1 } : {}}
+        transition={{ duration: 0.8 }}
       >
-        <>
-          <div
-            className={`section-heading ${
-              isVisible ? "in-view" : "out-of-view"
-            }`}
-          >
-            <span>{heading}</span>
-          </div>
-          <div
-            className={`section-content ${
-              isVisible ? "in-view" : "out-of-view"
-            }`}
-          >
-            {children}
-          </div>
-        </>
-      </section>
-    </>
+        {children}
+      </motion.div>
+    </motion.section>
   );
 };
 
